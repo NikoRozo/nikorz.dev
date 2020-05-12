7ba797a5421dad687b3fc53e936a7920d657859d
Nicolás E. Rozo Espinosa <niko.rozo92@outlook.com>
2020-05-12 18:34:46 -0500

    Test Netlify

--- src/templates/author.js
+++ src/templates/author.js
@@ -31,7 +31,7 @@ const Author = ({ data, location, pageContext }) => {
                             <h1>{author.name}</h1>
                             {author.bio && <p>{author.bio}</p>}
                             <div className="author-header-meta">
-                                {author.website && <a className="author-header-item" href={author.website} target="_blank" rel="noopener noreferrer">Website</a>}
+                                {author.website && <a className="author-header-item" href={author.website} target="_blank" rel="noopener noreferrer">Website!!!</a>}
                                 {twitterUrl && <a className="author-header-item" href={twitterUrl} target="_blank" rel="noopener noreferrer">Twitter</a>}
                                 {facebookUrl && <a className="author-header-item" href={facebookUrl} target="_blank" rel="noopener noreferrer">Facebook</a>}
                             </div>
