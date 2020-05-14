52ed285107e21bff5316209afbe7adcfc98502b3
Nicolás E. Rozo Espinosa <niko.rozo92@outlook.com>
2020-05-14 11:51:44 -0500

    Ajustes nombres de Site

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,15 +1,15 @@
 module.exports = {
     siteUrl: `https://www.nikorz.dev/`, // Site domain. Do not include a trailing slash!
 
-    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+    postsPerPage: 6, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Blog. Niko E. Rozo E.`, // This allows an alternative site title for meta data for pages.
+    siteTitleMeta: `NikoRz.Dev`, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `Blog de Desarrollo de App en Español`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Blog. Niko`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `NikoRz.Dev`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon-nikorz.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
