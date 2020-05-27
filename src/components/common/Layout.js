2b24aab9d5bc9934795a345bdc6f7ccd176fe91d
Niko Rozo <niko.rozo92@outlook.com>
2020-05-27 17:05:19 -0500

    Ajuste Layout Home

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -72,46 +72,48 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
 
                     <main className="site-main">
                         { isHome ?
-                        <div className="home-full-content">
-                            <div className="post-content-inner">
-                                {/* All the main content gets inserted here, index.js, post.js */}
-                                {children}
-                            </div>
-                            <div className="post-content-sidebar">
-                                <div className="home-description">
-                                    <h2>Post Destacados ✨</h2>
-                                    <p>
-                                    Estaré informando y recopilando las actualizaciones a 
-                                    los recursos publicados en los siguientes posts 👍🏻
-                                    </p>
+                        <div className="home_container">
+                            <div className="home-full-content">
+                                <div className="post-content-inner">
+                                    {/* All the main content gets inserted here, index.js, post.js */}
+                                    {children}
                                 </div>
+                                <div className="post-content-sidebar">
+                                    <div className="home-description">
+                                        <h2>Post Destacados ✨</h2>
+                                        <p>
+                                        Estaré informando y recopilando las actualizaciones a 
+                                        los recursos publicados en los siguientes posts 👍🏻
+                                        </p>
+                                    </div>
 
-                                <div className="home-links">
-                                    <Link to="/rompe-tus-limites">Rompe tus Limites!!!</Link><br/>
-                                    <Link to="/aws">Todo de AWS</Link><br/>
-                                    <Link to="/java">Todo de Java</Link><br/>
-                                    <Link to="/docker">Todo de Docker</Link><br/>
-                                    <Link to="/angular">Todo de Angular</Link><br/>
-                                    <Link to="/node-js">Todo de Node.js</Link>
-                                </div>
+                                    <div className="home-links">
+                                        <Link to="/rompe-tus-limites">Rompe tus Limites!!!</Link><br/>
+                                        <Link to="/aws">Todo de AWS</Link><br/>
+                                        <Link to="/java">Todo de Java</Link><br/>
+                                        <Link to="/docker">Todo de Docker</Link><br/>
+                                        <Link to="/angular">Todo de Angular</Link><br/>
+                                        <Link to="/node-js">Todo de Node.js</Link>
+                                    </div>
 
-                                <div className="home-description">
-                                    <br/>
-                                    <p>
-                                        Sígueme en mis redes sociales, para estar informado 😊
-                                    </p>
-                                </div>
+                                    <div className="home-description">
+                                        <br/>
+                                        <p>
+                                            Sígueme en mis redes sociales, para estar informado 😊
+                                        </p>
+                                    </div>
 
-                                <div className="post-follow follow-twitter">
-                                    <a href="https://twitter.com/NikoRozoArch" target="_blank" rel="noopener noreferrer">
-                                        <img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /> Sígueme
-                                    </a>
-                                </div>
+                                    <div className="post-follow follow-twitter">
+                                        <a href="https://twitter.com/NikoRozoArch" target="_blank" rel="noopener noreferrer">
+                                            <img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /> Sígueme
+                                        </a>
+                                    </div>
 
-                                <div className="post-follow follow-facebook">
-                                    <a href="https://facebook.com/NikoRz-104620944605700" target="_blank" rel="noopener noreferrer">
-                                        <img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /> Sígueme
-                                    </a>
+                                    <div className="post-follow follow-facebook">
+                                        <a href="https://facebook.com/NikoRz-104620944605700" target="_blank" rel="noopener noreferrer">
+                                            <img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /> Sígueme
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
